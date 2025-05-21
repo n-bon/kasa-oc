00c464a7f16aac6aba45e680b4dbae8abadb9935
Nathan Bonin <n.bonin@gmx.fr>
2025-05-21 23:01:30 +0200

    Style (assets) Add styles to header, clean scss files

--- src/assets/components/Header/index.jsx
+++ src/assets/components/Header/index.jsx
@@ -4,20 +4,20 @@ import '../../style/Header.scss'
 function Header () {
     const location = useLocation()
     return (
-        <header>
-            <p>Ceci est mon header</p>
+        <header className='navbar'>
             <img 
+            className='navbar__logo'
             src='src/assets/images/kasa-logo-color.png' 
             alt="logo de Kasa, platerforme de réservation d'hébergements" 
             />
-            <nav>
+            <nav className='navbar__menu'>
                 <Link 
                 to='/'
-                className={location.pathname === '/' ? 'navbar__link--active' : ''}
+                className={location.pathname === '/' ? 'navbar__link navbar__link--active' : 'navbar__link'}
                 >Accueil</Link>
                 <Link 
                 to='/a-propos'
-                className={location.pathname === '/a-propos' ? 'navbar__link--active' : ''}
+                className={location.pathname === '/a-propos' ? 'navbar__link navbar__link--active' : 'navbar__link'}
                 >À propos</Link>
             </nav>
         </header>
