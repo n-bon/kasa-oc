bc40962f33d9e2e34c7b0ec6a0b968466b31798b
Nathan Bonin <n.bonin@gmx.fr>
2025-05-21 23:25:04 +0200

    Style (Footer) Add styles desktop & mobile to footer

--- src/assets/components/Footer/index.jsx
+++ src/assets/components/Footer/index.jsx
@@ -1,9 +1,12 @@
-
+import '../../style/Footer.scss'
 function Footer() {
     return (
-        <footer>
-            <img src='src/assets/images/kasa-logo-white.png'  alt="logo de Kasa, réservation d'hébergements"/>
-            <p>© 2025 Kasa. All rights reserved</p>
+        <footer className='footer'>
+            <img 
+            className='footer__logo'
+            src='src/assets/images/kasa-logo-white.png'  
+            alt="logo de Kasa, réservation d'hébergements"/>
+            <p className='footer__text'>© 2025 Kasa. All rights reserved</p>
         </footer>
     )
 }
