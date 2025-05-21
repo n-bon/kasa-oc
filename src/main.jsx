9bff549547fbbc525690430ab4aec8ff246f3e79
Nathan Bonin <n.bonin@gmx.fr>
2025-05-21 09:51:21 +0200

    Build (main.jsx) Add first route to homepage

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
-  </StrictMode>,
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
+    </Router>
+  </StrictMode>
 )
