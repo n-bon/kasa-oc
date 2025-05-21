faf28e332333cfdb59437a8290e3c278caa818cc
Nathan Bonin <n.bonin@gmx.fr>
2025-05-21 18:58:26 +0200

    Style (app) Add montserrat font

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import Home from './pages/Home/'
 import About from './pages/About/'
 import Housing from './pages/Housing/'
 import Error from './pages/Error/'
+import './assets/style/main.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
