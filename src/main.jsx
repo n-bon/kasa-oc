e2b989427a2b7f264f0e8f5e60500491b36a8636
Nathan Bonin <n.bonin@gmx.fr>
2025-05-21 10:15:58 +0200

    Build (src) Add About page structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,23 @@
+//Import hooks from libraries here
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+//Import components here
 import Header from './assets/components/Header'
 import Footer from './assets/components/Footer'
+
+//Import pages here
 import Home from './pages/Home/'
+import About from './pages/About'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Header />
       <Routes>
-        <Route path='/' element={<Home />}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/a-propos' element={<About />} />
       </Routes>
       <Footer />
     </Router>
