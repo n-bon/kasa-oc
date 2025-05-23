b4d4eb865bcd0c6f5f82dbf577d8196257864676
Nathan Bonin <n.bonin@gmx.fr>
2025-05-23 23:50:18 +0200

    Build (Thumb) Add static desktop thumb component

--- src/assets/components/Thumb/index.jsx
+++ src/assets/components/Thumb/index.jsx
@@ -1,8 +1,21 @@
+import '../../style/Thumb.scss'
+
 function Thumb() {
     return (
-        <div>
-            <p>Ceci est ma carte</p>
-        </div>
+        <article 
+        className='thumb' 
+        key=''
+        >
+            <img 
+            class='thumb__background'
+            src='https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg'
+            />
+            <div class='thumb__layout'>
+                <h2 class='thumb__title'>
+                    Ceci est le titre de ma carte
+                </h2>
+            </div>
+        </article>
     )
 }
 
