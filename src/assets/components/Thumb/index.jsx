9826ed2c7b1e98a4bf2284bcbb55150dcb0c1e83
Nathan Bonin <n.bonin@gmx.fr>
2025-05-23 22:31:45 +0200

    Build (Home) Add thumbs structure

---
+++ src/assets/components/Thumb/index.jsx
@@ -0,0 +1,9 @@
+function Thumb() {
+    return (
+        <div>
+            <p>Ceci est ma carte</p>
+        </div>
+    )
+}
+
+export default Thumb
---
+++ src/assets/components/ThumbsGrid/index.jsx
@@ -0,0 +1,11 @@
+import Thumb from "../Thumb";
+
+function ThumbsGrid () {
+    return (
+        <section>
+            <Thumb />
+        </section>
+    )
+}
+
+export default ThumbsGrid
