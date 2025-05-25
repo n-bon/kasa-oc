573ccfb0f122db5bd4a0291b9fe88532b9f6d799
Nathan Bonin <n.bonin@gmx.fr>
2025-05-26 00:14:45 +0200

    Build (App) Add Housing page strucutre & adapt Collapse component

--- src/assets/components/Collapse/index.jsx
+++ src/assets/components/Collapse/index.jsx
@@ -5,6 +5,20 @@ function Collapse ({ title, content }) {
     const [isOpen, setIsOpen] = useState(false)
     const toggleCollapse = () => setIsOpen(!isOpen)
 
+    //if content is an array, transform it into a list
+    function renderContent() {
+        if (Array.isArray(content)) {
+            return (
+                <ul>
+                    {content.map((item, index) =>(
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            )
+        }
+        return content
+    }
+ 
     return (
         <div className='collapse'>
             <div className='collapse__head' onClick={toggleCollapse}>
@@ -12,7 +26,7 @@ function Collapse ({ title, content }) {
                 <i className={`collapse__icon fa-solid fa-chevron-up ${isOpen ? 'collapse__icon--open' : ''}`}></i>
             </div>
             <div className={`collapse__content ${isOpen ? 'collapse__content--open' : ''}`}>
-                {content}
+                {renderContent()}
             </div>
         </div>
     )
--- src/pages/Housing/index.jsx
+++ src/pages/Housing/index.jsx
@@ -2,25 +2,48 @@ import { useParams, Navigate } from 'react-router-dom'
 import housings from '../../../public/data/housings.json'
 
 import Slideshow from '../../assets/components/Slideshow'
+import Collapse from '../../assets/components/Collapse'
 
 function Housing() {
     const { id } = useParams()
     const housingItem = housings.find((item) => item.id === id)
-    console.log(housingItem)
+
     if (!housingItem) {
         return (
             <Navigate to='/erreur' replace />
         )
     }
+
     return (
         <main>
             <Slideshow
             pictures={housingItem.pictures}
             title={housingItem.title}
             />
-            <h1>{housingItem.title}</h1>
-            <p>{housingItem.id}</p>
-            <p>{housingItem.description}</p>
+            <section className='housing'>
+                <div className='housing__head'>
+                    <h1 className='housing__title'>{housingItem.title}</h1>
+                    <p className='housing__location'>{housingItem.location}</p>
+                    {/* Insérer les tags ici*/}
+                </div>
+                <div>
+                    {/* Insérer le profile sumup ici*/}
+                    {/* Insérer la note ici, gérer les propriétés avec flex */}
+                </div>
+            </section>
+            <section className='details'>
+                <Collapse
+                key='collapse-1'
+                title='Description'
+                content={housingItem.description}
+                />
+                <Collapse 
+                key='collapse-2'
+                title='Équipements'
+                content={housingItem.equipments}
+                />
+
+            </section>
         </main>
     )
 }
