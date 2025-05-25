f5e2fe29bdd795e7b9f6dc24467a568e2c2c55c3
Nathan Bonin <n.bonin@gmx.fr>
2025-05-25 22:02:01 +0200

    Build (slideshow) Add sliding programm

--- src/assets/components/Slideshow/index.jsx
+++ src/assets/components/Slideshow/index.jsx
@@ -6,6 +6,18 @@ function Slideshow({ pictures, title }) {
     //Slideshow management init & programm
     const [count, setCount] = useState(0)
 
+    function previousImage() {
+        setCount((prevCount) => 
+            prevCount === 0 ? pictures.length -1 : prevCount -1
+        )
+    }
+
+    function nextImage() {
+        setCount((prevCount) =>
+            prevCount === pictures.length - 1 ? 0 : prevCount + 1 
+        )
+    }
+
     //no slodeshow if 1 picture in array
     if (pictures.length === 1) {
         return (
@@ -23,14 +35,20 @@ function Slideshow({ pictures, title }) {
     return (
         <section className='slideshow'>
             <img 
-            src={`${pictures[0]}`} 
+            src={`${pictures[count]}`} 
             alt={`Aperçu de ${title}`}
             className='slideshow__picture'
             />
             <div className='slideshow__layout'>
                 <div className='slideshow__chevron'>
-                    <i className="slideshow__chevron--left fa-solid fa-chevron-left"></i>
-                    <i className="slideshow__chevron--right fa-solid fa-chevron-right"></i>                    
+                    <i 
+                        className="slideshow__chevron--left fa-solid fa-chevron-left"
+                        onClick={previousImage}
+                    ></i>
+                    <i 
+                        className="slideshow__chevron--right fa-solid fa-chevron-right"
+                        onClick={nextImage}
+                    ></i>                    
                 </div>
                 <div className='slideshow__count'>
                     <p className='slideshow__count'>{count + 1} / {pictures.length}</p>
