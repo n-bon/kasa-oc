bb9792c91a3d0d429003056d415fd4644d212f2c
Nathan Bonin <n.bonin@gmx.fr>
2025-05-31 19:17:13 +0200

    Perf (src) Delete thumbsgrid component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,29 @@
+//import components
 import Banner from '../../assets/components/Banner'
+import Thumb from '../../assets/components/Thumb'
+
+//import assets
+import data from '../../../public/data/housings.json'
 import backgroundImage from '../../../public/images/ocean.jpg'
-import ThumbsGrid from '../../assets/components/ThumbsGrid'
+
+//import style
+import './style.scss'
 
 function Home() {
 
   return (
     <main>
       <Banner title='Chez vous, partout et ailleurs' image={backgroundImage}/>
-      <ThumbsGrid />
+      <section className="grid">
+        {data.map((housing) => (
+            <Thumb 
+                key={housing.id}
+                id={housing.id}
+                title={housing.title}
+                cover={housing.cover}
+            />
+        ))}
+      </section>
     </main>
   )
 }
