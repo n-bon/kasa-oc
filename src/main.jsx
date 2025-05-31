b0bf1fd22e4da1a0de7d358f676d89b7c632976f
Nathan Bonin <n.bonin@gmx.fr>
2025-05-31 19:23:07 +0200

    Perf (src) Delete routing component, moved into main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,32 @@
 //Import hooks from libraries here
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 //Import components here
-import Routing from './assets/components/Router'
+import Header from './assets/components/Header'
+import Footer from './assets/components/Footer'
+
+//import pages here
+import Home from './pages/Home'
+import About from './pages/About'
+import Housing from './pages/Housing'
+import Error from './pages/Error'
 
 //import main style
 import './assets/style/main.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Routing />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/a-propos' element={<About />} />
+        <Route path='/hebergement/:id' element={<Housing />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
   </StrictMode>
 )
